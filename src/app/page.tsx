2672942a02f3e6b87d36e85173b05999a3ac8b04
Liam Davies <mangobox228@hotmail.com.au>
2025-05-22 21:32:12 +1000

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,105 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+
+import postgres from 'postgres';
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+interface Presentation {
+  title : string;
+  name : string;
+  pfp : string;
+}
+
+const pres = {
+  title : "Presentation 1",
+  name : "John Presentation",
+  pfp : "https://community.adobe.com/t5/image/serverpage/image-id/636919i6D8593BB232D17E4?v=v2"
+}
+
+function sendVote(score : number) {
+
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const boxes = [1, 2, 3].reverse()
+  
+  const [dialog, setDialog] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [presentation, setPresentation] = useState(pres as Presentation | null);
+  const [confirmDialog, setConfirmDialog] = useState(0);
+
+
+  return (
+    <>
+    {
+      confirmDialog ? 
+      <div className="modal is-active">
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Are you sure you want to give {confirmDialog} points?</p>
+          </header>
+          <footer className="modal-card-foot">
+            <div className="buttons">
+              <button className="button" onClick={() => setConfirmDialog(0)}>nope</button>
+              <button className="button is-success" onClick={() => sendVote(confirmDialog)}>yep</button>
+            </div>
+          </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+      : <></>
+    }
+    {
+    presentation ?
+      <div className="container content has-text-centered">
+        <div className="box is-size-4">
+          ⭐ Special Interest Presentations ⭐
+        </div>
+        <div className="section">
+          <div className="columns is-centered">
+            <div className="column">
+              <figure className="image is-128x128 centered is-inline-block">
+                <img src={presentation?.pfp}/>
+              </figure>
+              <h1 className="title"> {presentation?.title} </h1>
+              <p className="is-size-5">by {presentation?.name}</p>
+              <div className="box">
+                <p className="is-size-5">✏️ Select Score</p>
+                {
+                  boxes.map((box) => {
+                    return (
+                      <div key={box}>
+                        <button className="button center is-large" onClick={() => {setConfirmDialog(box)}}> {box} Points </button>
+                      </div>)
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+       : 
+      <div className="container content has-text-centered">
+        <div className="box is-size-4">
+          ⭐ Special Interest Presentations ⭐
+        </div>
+        <div className="section">
+          <div className="columns is-centered">
+            <div className="column">
+              <h2>
+                The next presentation will be starting soon!
+              </h2>
+              <figure className="image is-128x128 centered is-inline-block">
+                  <img src="https://media.tenor.com/NFO5znOGUzwAAAAM/black-kid-black.gif"/>
+                  
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+      }
+    </>
+  )
+
 }
